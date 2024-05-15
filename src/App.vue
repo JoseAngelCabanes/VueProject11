7804c710d78e33d1a0f794d3243ea0cb25bf09a0
JoseAngelCabanes <joseangelcabanes29@gmail.com>
2024-05-15 16:07:55 +0200

    added data insde script and first friend as an object

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,18 @@
 <template></template>
 
-<script></script>
+<script>
+const app = {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: 'Manuel Lorenz',
+          phone: '0123 45678 91',
+          email: 'manue@localhost.com'
+        },
+      ],
+    };
+  },
+};
+</script>
