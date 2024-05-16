da3bdc3fcbb2e6120b08a4bbbfd202281b3d7e33
JoseAngelCabanes <joseangelcabanes29@gmail.com>
2024-05-16 17:05:43 +0200

    added export friend contact and method toogleDetails

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button>Show Details</button>
+        <ul>
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        detailsAreVisible: false,
+        friend: {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0123 45678 91",
+          email: "manue@localhost.com",
+        },
+    }
+  },
+  methods: {
+    toggleDetails() {
+        this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+};
+</script>
