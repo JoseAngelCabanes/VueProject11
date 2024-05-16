268e8f201ab2d9930ae2bee47f78ded2f38b6c87
JoseAngelCabanes <joseangelcabanes29@gmail.com>
2024-05-16 13:37:24 +0200

    added template

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
-<template></template>
+<template>
+<section>
+  <h2>My friends</h2>
+  <ul>
+    <li></li>
+  </ul>
+</section>
+</template>
 
 <script>
-const app = {
+export default {
   data() {
     return {
       friends: [
         {
           id: "manuel",
-          name: 'Manuel Lorenz',
-          phone: '0123 45678 91',
-          email: 'manue@localhost.com'
+          name: "Manuel Lorenz",
+          phone: "0123 45678 91",
+          email: "manue@localhost.com",
         },
         {
           id: "julie",
-          name: 'Julie Jones',
-          phone: '0987 65432 10',
-          email: 'julie@localhost.com'
+          name: "Julie Jones",
+          phone: "0987 65432 10",
+          email: "julie@localhost.com",
         },
       ],
     };
