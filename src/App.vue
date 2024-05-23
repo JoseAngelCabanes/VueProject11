a109c0285729a901390a8a5013dd96b27ca2b2ec
JoseAngelCabanes <joseangelcabanes29@gmail.com>
2024-05-23 10:49:46 +0200

    added styles to App.vue for input, form, labels and divs

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ npm init vue uses an official package to help you initialize Vue projects. You g
   <section>
     <header><h1>My friends</h1></header>
     <form>
-      
+      <new-friend></new-friend>
     </form>
     <ul>
       <friend-contact
@@ -100,7 +100,8 @@ header {
   list-style: none;
 }
 
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -132,5 +133,21 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+
+#app form div {
+  margin: 1rem 0;
 }
 </style>
